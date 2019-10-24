1c334139f53a1c71480beb11af26777c23169cbb
s426242 <tymek.pawel@gmail.com>
2019-10-24 18:54:19 +0200

    Add second viewController for notepad app

---
+++ EncryptedNotepad_v2.0/NoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NoteViewController.swift
+//  EncryptedNotepad_v2.0
+//
+//  Created by xxx on 24/10/2019.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
