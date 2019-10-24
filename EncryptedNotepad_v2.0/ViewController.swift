47d87e30fc666e272663a30d24f323d394baf34e
s426242 <tymek.pawel@gmail.com>
2019-10-24 18:06:36 +0200

    Initial Commit

---
+++ EncryptedNotepad_v2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EncryptedNotepad_v2.0
+//
+//  Created by xxx on 24/10/2019.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
